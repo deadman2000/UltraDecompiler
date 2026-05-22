36ec161bf0b1182d0065bee1664f4667ae3430e8
Marchukov Ivan <marchukov.ivan@gmail.com>
2026-05-22 14:34:38 +0300

    Add Function class

---
+++ UltraDecompiler/Function.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace UltraDecompiler;
+
+/// <summary>
+/// Представляет одну функцию/процедуру в программе
+/// </summary>
+public class Function
+{
+    public int EntryPoint { get; set; }
+    public string Name { get; set; } = "sub_????";
+    public List<BasicBlock> Blocks { get; set; } = new();
+    public List<int> Callers { get; set; } = new();   // адреса, откуда вызывают эту функцию
+
+    public override string ToString()
+    {
+        return $"{Name} (0x{EntryPoint:X6}) - {Blocks.Count} blocks";
+    }
+}
