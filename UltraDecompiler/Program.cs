ac5e437147714bec9b2deb5aa0e7732f2eed2abe
Marchukov Ivan <marchukov.ivan@gmail.com>
2026-05-22 08:54:03 +0300

    Update Program.cs to demonstrate the new disassembler

--- UltraDecompiler/Program.cs
+++ UltraDecompiler/Program.cs
@@ -1,4 +1,30 @@
-﻿using UltraDecompiler;
+using UltraDecompiler;
 
-var parser = new DosExeParser(@"D:\Projects\TranslateWeb\Neuromancer\Neuromancer_re\NEURO.EXE");
-parser.PrintInfo();
+if (args.Length == 0)
+{
+    Console.WriteLine("Usage: UltraDecompiler <path_to_dos_exe>");
+    Console.WriteLine("Example: UltraDecompiler.exe game.exe");
+    return;
+}
+
+string exePath = args[0];
+
+try
+{
+    var parser = new DosExeParser(exePath);
+    parser.PrintInfo();
+
+    Console.WriteLine("\n=== Disassembly from entry point ===");
+
+    var disassembler = new X86Disassembler(parser.Image);
+    var instructions = disassembler.Disassemble((int)parser.EntryPointOffset, maxInstructions: 80);
+
+    foreach (var instr in instructions)
+    {
+        Console.WriteLine(instr);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
