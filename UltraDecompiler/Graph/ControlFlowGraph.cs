87b86d388366cd280e249d0b03782c759523770c
Marchukov Ivan <marchukov.ivan@gmail.com>
2026-05-24 13:24:34 +0300

    Реализовал метод SaveDot в ControlFlowGraph: узлы как блоки с ASM-кодом, цветные стрелки (чёрная для одного перехода, зелёная/красная для двух)

--- UltraDecompiler/Graph/ControlFlowGraph.cs
+++ UltraDecompiler/Graph/ControlFlowGraph.cs
@@ -1,4 +1,6 @@
 using UltraDecompiler.Disassembler;
+using System.IO;
+using System.Text;
 
 namespace UltraDecompiler.Graph;
 
@@ -81,7 +83,68 @@ public class ControlFlowGraph
     /// <param name="fileName"></param>
     public void SaveDot(string fileName)
     {
-        // TODO
+        if (Blocks.Count == 0)
+        {
+            File.WriteAllText(fileName, "digraph empty {}");
+            return;
+        }
+
+        var sb = new StringBuilder();
+        sb.AppendLine("digraph ControlFlowGraph {");
+        sb.AppendLine("    rankdir=TB;");
+        sb.AppendLine("    node [shape=box, fontname=\"Consolas\", fontsize=8, style=filled, fillcolor=\"#FFFACD\"];");
+        sb.AppendLine("    edge [fontname=\"Consolas\", fontsize=7];");
+
+        // Узлы - блоки с ассемблерным кодом
+        foreach (var block in Blocks.OrderBy(b => b.StartOffset))
+        {
+            string nodeId = $"b_{block.StartOffset:X6}";
+            var lines = new List<string> { $"0x{block.StartOffset:X4}..0x{block.EndOffset:X4}" };
+            foreach (var instr in block.Instructions)
+            {
+                string line = instr.ToString().Replace("\\", "\\\\").Replace("\"", "\\\"");
+                lines.Add(line);
+            }
+            string label = string.Join("\\l", lines) + "\\l";
+            sb.AppendLine($"    {nodeId} [label=\"{label}\"];");
+        }
+
+        // Стрелки: один переход - чёрная, два - зелёная (taken) и красная (fallthrough)
+        foreach (var block in Blocks)
+        {
+            string from = $"b_{block.StartOffset:X6}";
+            bool hasTwo = block.NextBlock != null && block.ConditionalBlock != null;
+
+            if (block.NextBlock != null)
+            {
+                string to = $"b_{block.NextBlock.StartOffset:X6}";
+                if (hasTwo)
+                {
+                    sb.AppendLine($"    {from} -> {to} [color=red, label=\"fallthrough\"];");
+                }
+                else
+                {
+                    sb.AppendLine($"    {from} -> {to} [color=black];");
+                }
+            }
+
+            if (block.ConditionalBlock != null)
+            {
+                string to = $"b_{block.ConditionalBlock.StartOffset:X6}";
+                if (hasTwo)
+                {
+                    sb.AppendLine($"    {from} -> {to} [color=green, label=\"taken\"];");
+                }
+                else
+                {
+                    sb.AppendLine($"    {from} -> {to} [color=green];");
+                }
+            }
+        }
+
+        sb.AppendLine("}");
+        File.WriteAllText(fileName, sb.ToString());
+        Console.WriteLine($"CFG saved to {fileName}");
     }
 
     /// <summary>
