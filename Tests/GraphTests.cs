54181e0e2f7195539787eb3d0b105b2f7bf53d8a
Иван Марчуков <min@ntc-platforma.ru>
2026-05-25 22:24:18 +0300

    Тест на CFG

---
+++ Tests/GraphTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Tests;
+
+public class GraphTests : BaseTests
+{
+    [Fact]
+    public void TwoBlocks()
+    {
+        var graph = GetGraph("""
+            B8 01 00 ; mov ax, 1
+            EB 01    ; jmp short +1
+            90       ; nop (в следующем блоке)
+            """);
+
+        Assert.Equal(2, graph.Blocks.Count);
+    }
+}
