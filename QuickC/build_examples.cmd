61c13a3b1da14f8b20f6d3cb024633e662b55759
Иван Марчуков <min@ntc-platforma.ru>
2026-06-09 21:10:58 +0300

    ручная сборка примеров

--- QuickC/build_examples.cmd
+++ QuickC/build_examples.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 
-dosbox-x -conf dosbox.conf -nopromptfolder -fastlaunch -silent -c "make" > build.log 2>&1
+dosbox-x -conf dosbox.conf -nopromptfolder -fastlaunch -silent -c "make clean" -c "make" > build.log 2>&1
 type build.log
 del build.log
