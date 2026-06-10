176139c1cfa5a8c2cd90aeb7b4646474fb5b81fd
Иван Марчуков <min@ntc-platforma.ru>
2026-06-10 15:27:25 +0300

    мелкие правки

--- QuickC/build_decompiled.cmd
+++ QuickC/build_decompiled.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 
-dosbox-x -conf dosbox.conf -nopromptfolder -fastlaunch -silent -c "CD C:\QuickC\programs\decomp~1" -c "make" > build.log 2>&1
+dosbox-x -conf dosbox.conf -nopromptfolder -fastlaunch -silent -c "CD C:\QuickC\programs\decomp~1" -c "make clean" -c "make" > build.log 2>&1
 type build.log
 del build.log
